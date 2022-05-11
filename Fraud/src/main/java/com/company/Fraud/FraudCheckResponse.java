abe2a70912ada6e9aa2b390178d6765eeb4aed43
“DawidSkra007” <dawid.skraba@ucdconnect.ie>
2022-05-11 17:42:29 +0100

    Fraud Microservice

---
+++ Fraud/src/main/java/com/company/Fraud/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package com.company.Fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+
+}
---
+++ Fraud/src/main/java/com/company/Fraud/FraudCheckService.java
@@ -0,0 +1,25 @@
+package com.company.Fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class FraudCheckService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public boolean isFraudulentCustomer(Integer customerId) {
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraud(false)
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+        // LOGIC
+        return false;
+    }
+}
